d4ef3bf3930c1a93239b8b969756a5ead99c4c9a
Jose Luis <jolui334@gmail.com>
2021-04-23 22:33:24 -0500

    Loop through data has been included. D3 row appender has been included too

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,44 @@
 // from data.js
-var tableData = data;
+let tableData = data;
 
 // YOUR CODE HERE!
+
+//2. Creating the variable that will be used to search data on an specific date
+
+
+// 1. Creating the functions that will populate the table
+
+let DateInput = "1/8/2010"
+
+// D3 object that will be updating the table
+let tbody = d3.select("tbody")
+
+// Button to click to refresh the data
+var button = d3.select("#click-me");
+
+//  Input tha will be used to get the user date input
+
+var inputField = d3.select("#input-field");
+//Arrow function to iterate over the selected data
+tableData.forEach(obs => {
+
+    console.log(obs);
+    console.log("-----New obj----" )
+    let row = tbody.append("tr");
+    console.log(obs.datetime)
+
+    // Conditional to check against user input
+    if (obs.datetime === DateInput){
+
+    // Only data within the user input will be used
+
+        console.log("Date Found")
+        Object.entries(obs).forEach(([key, value]) =>{
+            // console.log(`key: ${key}, value:${value}`);
+            var cell = row.append("td")
+            cell.text(value)
+        });
+     }  
+    
+});
+
